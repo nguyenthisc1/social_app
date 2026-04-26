67ab5d167092026a7bf9019f8cab29040c569a7e
Thi Nguyễn <nguyenthisc1@gmail.com>
2026-04-26 15:43:22 +0700

    update exception notification feature

--- lib/features/notification/domain/notification_exeptions.dart
+++
@@ -1,5 +0,0 @@
-import 'package:social_app/core/domain-base/exceptions/exception-base.dart';
-
-class NotificationExeptions extends ExceptionBase {
-  NotificationExeptions({required super.message});
-}
